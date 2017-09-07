ed490faa6e00f8d011281ed2314c0f0cf117d6a8
MacBlackCat <jelmer.oldersma@gmail.com>
2017-09-07 22:39:02 +0200

    Cleaned LoadPage js a bit

--- CiosWorkshops/www/scripts/loadPage.js
+++ CiosWorkshops/www/scripts/loadPage.js
@@ -26,44 +26,6 @@ function loadPage(page) {
 // Loads every event in database in the admin event menu, plus update function to activate/de-activate event active status
 function readEvents() {
     var eventsRef = firebase.database().ref('/evenementen/');
-    /*firebase.database().ref('/evenementen/').once('value').then(function (snapshot) {
-        var contentTable = "<table><tbody>"; //Start of table string
-        var eventObj = snapshot.val();
-
-        for (var eventsName in eventObj) { //Adds each event to table string
-            contentTable += "<tr><td><h3>" + eventsName + "</h3></td><td id='" + eventsName + "Id'></td ></tr > "; //Table code
-            eventsMade.push(eventsName); //To keep track of how many events + their names
-            
-            if (eventObj[eventsName].active == true) { //Check if event is active or not, pushes value into array for later use
-                eventsState.push(true);
-            } else if (eventObj[eventsName].active == false) {
-                eventsState.push(false);
-            } else {
-                console.log("Cant find active!");
-            }
-        }
-        $('#eventsDiv').append(contentTable + "</tbody></table>"); //Writes the table into the page
-
-        eventsMade.forEach(function (item, index) { //For each event in the array, adding function based on their active status (the eventsState array)
-            var eventsName = item;
-
-            if (eventsState[index] == true) { // Adds a switch to selected event, with correct Active status
-                $("#" + eventsName + "Id").html("<div class='switch small'><input class='switch-input' id='" + eventsName + "Switch' checked type='checkbox'><label class='switch-paddle' for='" + eventsName + "Switch'></div>");
-            } else if (eventsState[index] == false) {
-                $("#" + eventsName + "Id").html("<div class='switch small'><input class='switch-input' id='" + eventsName + "Switch' type='checkbox'><label class='switch-paddle' for='" + eventsName + "Switch'></div>");
-            } else {
-                console.log("Couldnt give checkmark");
-            }
-
-            $("#" + eventsName + "Switch").on("click", function (e) { //Adds click function on the switch element, in order to update active status in database
-                if ($("#" + eventsName + "Switch").prop("checked")) { //Check if switch element has the 'checked' property
-                    firebase.database().ref('/evenementen/' + eventsName).update({ active: true });
-                } else {
-                    firebase.database().ref('/evenementen/' + eventsName).update({ active: false });
-                }
-            });
-        });
-    });*/
 
     eventsRef.on('value', function (snapshot) {
         var eventsMade = [];
@@ -75,7 +37,7 @@ function readEvents() {
             contentTable += "<tr><td><h3>" + eventsName + "</h3></td><td id='" + eventsName + "Id'></td ></tr > "; //Table code
             eventsMade.push(eventsName); //To keep track of how many events + their names
 
-            if (eventObj[eventsName].active == true) { //Check if event is active or not, pushes value into array for later use
+            if (eventObj[eventsName].active == true) { //Check if event is active or not, pushes value into array for later use (aka the switches)
                 eventsState.push(true);
             } else if (eventObj[eventsName].active == false) {
                 eventsState.push(false);
@@ -85,6 +47,7 @@ function readEvents() {
         }
         $('#eventsDiv').html(contentTable + "</tbody></table>"); //Writes the table into the page
 
+        //Gives each Event its own buttons
         eventsMade.forEach(function (item, index) { //For each event in the array, adding function based on their active status (the eventsState array)
             var eventsName = item;
 
