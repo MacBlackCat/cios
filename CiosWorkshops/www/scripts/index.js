d38ef81b0dcdfb6b96183e85a41ea25aadc70753
MacBlackCat <jelmer.oldersma@gmail.com>
2017-09-07 15:29:10 +0200

    derp

--- CiosWorkshops/www/scripts/index.js
+++ CiosWorkshops/www/scripts/index.js
@@ -18,7 +18,7 @@
                 var displayName = user.displayName;
                 //var email = user.email;
                 //var emailVerified = user.emailVerified;
-                var photoURL = user.photoURL;
+                //var photoURL = user.photoURL;
                 //var isAnonymous = user.isAnonymous;
                 var uid = user.uid;
                 var providerData = user.providerData;
